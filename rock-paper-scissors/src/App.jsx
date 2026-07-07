a5dd59c1d17045603206cea663084dc7d2c4d414
Misko <m.hresko.dev@gmail.com>
2026-07-07 20:35:45 +0200

    Create user input and output user vs computer option

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,10 +1,27 @@
 import getComputerChoice from "./components/getComputerChoice";
 
 const App = () => {
+  // user input his choice
+  let person = prompt("Please enter your choice: ");
+  // IF user wrote in big letters it will get converted
+  person = person.toLowerCase();
+  // IF user accidentally click space after/before word we remove it
+  person = person.split(" ").join("");
+  // Get random computer option
+  let computer = getComputerChoice();
+
+  // IF user use valid option show us info
+  if (person === "rock" || person === "paper" || person === "scissors") {
+    console.log(`Your choice: ${person} VS Computer choice: ${computer}`);
+  } // ELSE show message that we should enter valid value
+  else {
+    console.log("Invalid value! Choose from rock, paper, scissors!");
+  }
+
   return (
     <>
-      <button onClick={() => console.log(getComputerChoice())}>Play</button>
-      {/* <p>{result}</p> */}
+      <h1>Rock Paper Scissors</h1>
+      <p>Open console F12</p>
     </>
   );
 };
